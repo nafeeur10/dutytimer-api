a982e6048aa9a19d45c374811248f4263ddaa1c1
Nafeeur <inafeeur@gmail.com>
2022-09-26 19:42:51 +0600

    User created successfully

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function register(RegisterRequest $request)
     {
-        dd($request->validated());
+        $user = User::create($request->validated());
+        if($user) return response()->json([
+            'data' => $user,
+            'message' => 'User created successfully'
+        ], 201);
     }
 }
