eab333fcda1d026ec1466e1349b411ef406262df
Nafeeur <inafeeur@gmail.com>
2022-10-09 11:04:56 +0600

    Auth Controller functionality continue

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,12 +6,19 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function register(RegisterRequest $request)
     {
-        $user = User::create($request->validated());
+        $user = User::create([
+            'name'      => $request->name,
+            'email'     => $request->email,
+            'password'  => Hash::make($request->password)
+        ]);
         if($user) return response()->json([
             'data' => $user,
             'message' => 'User created successfully'
@@ -20,9 +27,33 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        $credentials = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
+        $validator = Validator::make($request->all(), [
+            'email'     => 'required|email',
+            'password'  => 'required',
         ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator,
+                'message' => 'Email or Password is not correct!'
+            ], 422);
+        }
+
+        /** @var User|null $user */
+        $user = User::firstWhere('email', $request->email);
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'errors' => $validator,
+                'message' => 'Email or Password is not correct!'
+            ], 422);
+        }
+
+        $token = $user->createToken(config('app.name'))->plainTextToken;
+        return response()->json([
+            'errors'  => null,
+            'token'   => $token,
+            'message' => 'Login Successful'
+        ], 200);
     }
 }
